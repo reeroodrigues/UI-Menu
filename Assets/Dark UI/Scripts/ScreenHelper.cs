0e2a57a8cc09c16c654c8dc4b601aefc51e8b91e
Renan Malveira Rodrigues <renan.pocket@gmail.com>
2022-12-26 18:04:29 -0300

    Menu UI

---
+++ Assets/Dark UI/Scripts/ScreenHelper.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Screens
+{
+    public class ScreenHelper : MonoBehaviour
+    {
+        public ScreenType screenType;
+
+        public void OnClick()
+        {
+            ScreenManager.Instance.ShowByType(screenType);
+        }
+
+    }
+
+}
